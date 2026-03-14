feb973f80868a8fa8ad22076a58699d4efdbd1fb
Rushi-Technologies <your.email@example.com>
2026-03-14 21:08:22 +0530

    Updated

--- src/main/java/com/rushi/controller/HomeController.java
+++ src/main/java/com/rushi/controller/HomeController.java
@@ -91,6 +91,6 @@ public class HomeController {
     }
 
     private List<String> getCourses() {
-        return Arrays.asList("DevOps", "AWS", "Azure Admin With Azure DevOps", "Terraform", "K8s");
+        return Arrays.asList("DevOps", "AWS", "Azure Admin With Azure DevOps", "Terraform", "K8s","Python");
     }
 }
